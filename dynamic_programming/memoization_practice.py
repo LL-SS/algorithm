f995531df917e4de015d96a61f3c473abbbaac70
이선우 <leeseonwoo@iseon-uui-MacBookAir.local>
2022-08-24 16:24:57 +0900

    220824

--- dynamic_programming/memoization_practice.py
+++ dynamic_programming/memoization_practice.py
@@ -1,6 +1,24 @@
 def max_profit_memo(price_list, count, cache):
     # 코드를 작성하세요.
+    if count < 2:
+        cache[count] = price_list[count]
+        return cache[count]
 
+    if count in cache:
+        return cache[count]
+
+    if count < len(price_list):
+        profit = price_list[count]
+
+    else:
+        profit = 0
+
+    for i in range(1, (count // 2) + 1):
+        profit = max(profit, max_profit_memo(price_list, i, cache) + max_profit_memo(price_list, count - i, cache))
+
+    cache[count] = profit
+
+    return cache[count]
 
 def max_profit(price_list, count):
     max_profit_cache = {}
---
+++ greedy_algorithm/course_selection.py
@@ -0,0 +1,14 @@
+def course_selection(course_list):
+    # 코드를 작성하세요.
+    selected_list = []
+
+    for course in course_list:
+        start, end = course[0], course[1]
+
+        for
+
+
+# 테스트
+print(course_selection([(6, 10), (2, 3), (4, 5), (1, 7), (6, 8), (9, 10)]))
+print(course_selection([(1, 2), (3, 4), (0, 6), (5, 7), (8, 9), (5, 9)]))
+print(course_selection([(4, 7), (2, 5), (1, 3), (8, 10), (5, 9), (2, 5), (13, 16), (9, 11), (1, 8)]))
