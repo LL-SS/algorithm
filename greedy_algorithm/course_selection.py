3b95d83e91afc5d7f94402ac6c7b1f7566166fca
ll-ss <leesw0529@gmail.com>
2022-08-24 21:24:07 +0900

    220824_2

--- greedy_algorithm/course_selection.py
+++ greedy_algorithm/course_selection.py
@@ -1,18 +1,17 @@
 def course_selection(course_list):
-    # 코드를 작성하세요.
-    answer_list = []
+    # 수업을 끝나는 순서로 정렬한다
+    sorted_list = sorted(course_list, key=lambda x: x[1])
 
-    course_list.sort(key=lambda x: (x[1], x[0]))
+    # 가장 먼저 끝나는 수업은 무조건 듣는다
+    my_selection = [sorted_list[0]]
 
-    answer_list.append(course_list[0])
-    start_time = course_list[0][1]
+    # 이미 선택한 수업과 안 겹치는 수업 중 가장 빨리 끝나는 수업을 고른다
+    for course in sorted_list:
+        # 마지막 수업이 끝나기 전에 새 수업이 시작하면 겹친다
+        if course[0] > my_selection[-1][1]:
+            my_selection.append(course)
 
-    for i in range(1, len(course_list)):
-        if start_time < course_list[i][0]:
-            answer_list.append(course_list[i])
-            start_time = course_list[i][1]
-
-    return answer_list
+    return my_selection
 
 
 # 테스트
