bcf72fb32918630afb45baa8ca47505f366fef91
LL-SS <86500315+LL-SS@users.noreply.github.com>
2023-05-16 08:44:30 +0900

    [level 0] Title: 소인수분해, Time: 0.25 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,18 @@
+def solution(n):
+    answer = []
+    
+    for i in range(2, n + 1):
+        if n % i == 0 and is_prime(i):
+            answer.append(i)
+    
+    return answer
+
+def is_prime(n):
+    if n < 2:
+        return False
+    
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    
+    return True
