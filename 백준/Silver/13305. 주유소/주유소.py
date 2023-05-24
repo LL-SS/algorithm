9164bf3b634f29d1b134d9bdecabdbd12ba0a066
LL-SS <86500315+LL-SS@users.noreply.github.com>
2023-05-24 11:45:51 +0900

    [Silver III] Title: 주유소, Time: 100 ms, Memory: 42684 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,24 @@
+inputs = []
+
+for i in range(3):
+    inputs.append(list(map(int, input().split(' '))))
+
+n, length, price = inputs[0][0], inputs[1], inputs[2]
+i = 0
+ans = 0
+
+while i < n:
+    to_end = price[i] * sum(length[i:])
+    each = 0
+
+    for j in range(i, n - 1):
+        each += price[j] * length[j]
+
+    if to_end <= each:
+        ans += to_end
+        i += n
+    else:
+        ans += price[i] * length[i]
+        i += 1
+
+print(ans)
